a493ce3bb762e28b8597aba6b75b1b68934acac5
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-06-12 19:46:29 -0300

    chore: added firebase dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    id ("com.google.devtools.ksp")
+    id ("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.casaque.pdmchat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.casaque.pdmchat"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -38,10 +41,23 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // room
+    implementation ("androidx.room:room-runtime:2.6.0")
+    annotationProcessor ("androidx.room:room-compiler:2.6.0")
+    ksp ("androidx.room:room-compiler:2.6.0")
+
+    // firebase
+    //implementation ("com.google.firebase:firebase-bom:32.5.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+
+    // realtime database
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-database-ktx")
 }
