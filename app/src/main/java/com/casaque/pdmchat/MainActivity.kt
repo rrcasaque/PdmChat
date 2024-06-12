f59366ed3d58e1449acb6608d742651b62925484
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2024-06-12 19:52:32 -0300

    feat: implemented main activity

--- app/src/main/java/com/casaque/pdmchat/MainActivity.kt
+++ app/src/main/java/com/casaque/pdmchat/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.casaque.pdmchat
 
+import MessageAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageRepository: MessageRepository
+    private lateinit var usernameInput: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        messageRepository = MessageRepository()
+
+        usernameInput = findViewById(R.id.username_input)
+        loginButton = findViewById(R.id.login_button)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        messageAdapter = MessageAdapter(emptyList())
+        recyclerView.adapter = messageAdapter
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            if (username.isNotBlank()) {
+                Log.d("MainActivity", "Loading messages for user: $username")
+                loadMessagesForUser(username)
+            } else {
+                Log.d("MainActivity", "Username is blank")
+            }
+        }
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, SendMessageActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loadMessagesForUser(username: String) {
+        messageRepository.getMessages(username) { messages ->
+            Log.d("MainActivity", "Updating adapter with ${messages.size} messages")
+            messageAdapter.updateMessages(messages)
+        }
     }
 }
